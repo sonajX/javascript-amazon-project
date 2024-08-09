d790638cdd41649f52ed12062bf3672c32abb62c
sonajX <jwalmendra@gmail.com>
2024-08-09 08:33:24 +0800

    delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 1
@@ -26,4 +26,13 @@ export function addToCart(productId) {
       quantity: 1
     })
   }
+}
+
+
+export function removeFromCart(productId) {
+  const newCart = cart.filter((item) => {
+    return !(item.productId === productId);
+  });
+
+  cart = newCart;
 }
